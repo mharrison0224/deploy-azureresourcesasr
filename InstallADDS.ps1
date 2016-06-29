d80194be7fff065c1346c0b8f736fea785413a13
mharrison0224 <mharrison0224@gmail.com>
2016-06-29 12:32:30 -0500

    Add files via upload

---
+++ InstallADDS.ps1
@@ -0,0 +1,6 @@
+﻿$NewDisk = Get-Disk | Where-Object PartitionStyle -eq "RAW"
+Initialize-Disk -Number $NewDisk.Number 
+New-Partition -DiskNumber $NewDisk.Number -DriveLetter N -UseMaximumSize
+Format-Volume -DriveLetter N -Confirm:$false -Force
+
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
