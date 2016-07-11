daf27be4fdde0b5cda2b4507ac0b3e4d64b0d1b7
mharrison0224 <mharrison0224@gmail.com>
2016-07-11 12:54:25 -0500

    Update InstallADDS.ps1

--- InstallADDS.ps1
+++ InstallADDS.ps1
@@ -1,6 +1,7 @@
-﻿$NewDisk = Get-Disk | Where-Object PartitionStyle -eq "RAW"
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
+
+$NewDisk = Get-Disk | Where-Object PartitionStyle -eq "RAW"
 Initialize-Disk -Number $NewDisk.Number 
 New-Partition -DiskNumber $NewDisk.Number -DriveLetter N -UseMaximumSize
 Format-Volume -DriveLetter N -Confirm:$false -Force
 
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
