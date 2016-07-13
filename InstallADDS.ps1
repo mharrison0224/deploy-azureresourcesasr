ff04429dd5b58eef3e0fb863f661302a8a9b925a
mharrison0224 <mharrison0224@gmail.com>
2016-07-13 10:10:16 -0500

    Update InstallADDS.ps1

--- InstallADDS.ps1
+++ InstallADDS.ps1
@@ -1,4 +1,7 @@
-<# Custom Script for Windows #>
-$folderName = (Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")            
-New-Item -itemType Directory -Path d:\ -Name $folderName
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
+
+$NewDisk = Get-Disk | Where-Object PartitionStyle -eq "RAW"
+Initialize-Disk -Number $NewDisk.Number 
+New-Partition -DiskNumber $NewDisk.Number -DriveLetter N -UseMaximumSize
+Format-Volume -DriveLetter N -Confirm:$false -Force
 
